92a1e2f5c9121210a66b4508032013b69ca85d4d
Ilyas <zhumadillov@gmail.com>
2020-06-18 11:53:15 +0300

    Initial Commit

---
+++ NewsVesti/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsVesti
+//
+//  Created by Ilyas Zhumadilov on 18.06.2020.
+//  Copyright © 2020 Ilyas Zhumadilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
